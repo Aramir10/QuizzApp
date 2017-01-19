7ea7d12b9cb408d64c825e6c919a4c0de493527b
Victor <victorgdl89@gmail.com>
2017-01-18 22:46:56 -0600

    Quizz app

---
+++ quizz.js
@@ -0,0 +1,60 @@
+
+var questions = [];
+var choices =  ["blue", "red", "yellow"] ;
+var choices1 = ["San Antonio", "Brownsville", "Helotes"];
+var choices2 = ["batman","superman","flash"];
+var choices3 = ["hey you","hola", "cheese"];
+var choices4 = [ "Java", "Python", "javascript"];
+
+var count = [];
+
+
+//Quiz App
+class QuizzClass {
+
+	 answer:string;
+	 choices:number;
+	 	constructor() 	{
+      console.log();
+			prompt.answer
+	}
+}
+// type numbers for answers
+var QuizzClass1 = new QuizzClass();
+QuizzClass1.questions = 'Whats you favorite color';
+
+if(choices ===  "red"||"yellow"){
+	count + 0
+	if ("blue") {
+		 count +1
+	} else {console.log("That is not an answer");
+
+	}
+console.log(QuizzClass1)};
+
+var QuizzClass2 = new QuizzClass();
+QuizzClass2.questions = 'Whats your favorite city';
+if(choices1 === "Brownsville"|| "Helotes"){
+	 count + 0
+	if ("San Antonio") {count +1
+	} else { alert("That is not an answer");
+
+	}
+console.log(QuizzClass2)};
+
+var QuizzClass3 = new QuizzClass();
+QuizzClass3.questions = 'Whats your favorite super hero';
+if(choices2 === "batman"||"superman"||"flash"){
+console.log(QuizzClass3)};
+
+
+var QuizzClass4 = new QuizzClass();
+QuizzClass4.questions = 'Whats your favorite song';
+if(choices3 === "hey you"|| "hola" || "cheese"){
+console.log(QuizzClass4);
+
+var QuizzClass5 = new QuizzClass();
+QuizzClass5.questions = 'Whats your favorite programing language';
+if(choices ===  "Java"|| "Python" || "javascript"){
+console.log(QuizzClass5);
+}
